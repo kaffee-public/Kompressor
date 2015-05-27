0e19004f355a1699709ecb19838144d8309c7ced
adamhorvath88 <adamhorvath88@yahoo.com>
2015-05-27 22:15:41 +0200

    KOMP-6 Documentation.

--- java/kaffee/kompressor/Display.java
+++ java/kaffee/kompressor/Display.java
@@ -10,7 +10,7 @@ class Display {
 
 	public static void about() {
 		System.out.println("Kompressor v0.1 - Compress PHP files to smaller size");
-		System.out.println("(c) 2014 Kaffee");
+		System.out.println("(c) 2015 Kaffee");
 		System.out.println();
 	}
 
